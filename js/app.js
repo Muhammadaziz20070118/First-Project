2e153c25cf3139cbd88f25d8661ec74bf9c360cd
Rahmonaliyev Muhammadaziz <118888739+Muhammadaziz20070118@users.noreply.github.com>
2022-11-23 12:41:13 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,80 @@
+
+
+// Binding   decloration    assignment
+    // let         name        =  10;
+// let name = 'usmonjon'
+// name = 'Usmonjon';
+
+// console.log(name);
+
+// const age = 21;
+// const age = 31;
+// // age = 31
+
+// console.log(age);
+
+
+// var surename = "Tokhirjonov";
+// console.log(surename);
+// var surename = "Obidov";
+// surename = 'Hello World!'
+// console.log(surename);
+
+// naming convention => o'zgaruvchilarni nomalash
+
+// Ruxsat berilmagan
+
+// 1. Agar O'zgaruvchini nomi JSdagi kalit so'z bilan bir xil bo'lsa nomlash mumkin
+// let if = 31;
+// else
+// true;
+// NaN;
+// Undefined;
+
+// 2 JS Operator
+// + - = * / ? < > ! &
+// let name+surename = 30;
+
+// let 1name = 'hello'
+// let name = hello
+// console.log(name);
+
+let sureName = 'Tokhirjonov'; // camelCase
+let surename = 'Salom';
+console.log(undefined + 5);
+
+
+// Data Type:
+// Primative Data Type 
+// 1. Number         => 45345345345
+// 2. String         => '', "", ``;
+// 3. NaN            => 
+// 4. undefined      =>
+// 5. null           =>
+
+// Reference 
+// Object
+// array
+// function
+
+const shaxs = {
+    ism: "Usmonjon",
+    familiya: 'Tokhirjonov',
+    yosh: 21,
+    dustlar: ['Sen', 'Men', 'Qushnim'],
+    otasi: {
+        name: 'Abdurasul'
+    }
+};
+
+// shaxs.yosh = 31;
+
+// console.log(shaxs.dustlar);
+        //    0         1       2         3
+let arr = ['Salom', 'hello', 'ket', [1, 2, 3]]
+
+arr[0] = 'World'
+arr[4] = 'hi';
+arr[6] = 'hi';
+arr[7] = shaxs;
+console.log(arr);
